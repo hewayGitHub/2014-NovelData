b4a2fbfd2dfa40a3a650c99063f546ba0f2da888
sunhaowen <sunhaowen123@gmai.com>
2014-02-12 15:40:47 +0800

    novel node generate

--- novel/cluster/ClusterNodeModule.py
+++ novel/cluster/ClusterNodeModule.py
@@ -5,6 +5,7 @@ __author__ = 'sunhaowen'
 __date__ = '2014-02-02 22:51'
 
 import logging
+from ConfigParser import SafeConfigParser
 
 from basic.NovelStructure import *
 from novel.cluster.ClusterDB import *
@@ -17,91 +18,58 @@ class ClusterNodeModule(object):
     """
         将各个站点的小说信息整合后更新到点集合中
     """
-    def __init__(self):
+    def init_time_info(self):
         """
-            初始化操作，读取配置
+        """
+        self.proc_time_dict = {}
+        for line in open('./data/NovelClusterModule.time', 'r').readlines():
+            (table, update_time) = line.strip().split(':')
+            self.proc_time_dict[table] = int(update_time)
+
+
+    def init_log_info(self):
+        """
         """
         self.logger = logging.getLogger('novel.cluster.node')
         self.err = logging.getLogger('err.cluster.node')
 
-    def generate_novel_node_list(self, site_id, update_time):
+
+    def init_conf_info(self):
         """
-            更新site_id站点中，更新时间大于update_time的小说信息
         """
-        cluster_db = ClusterDBModule()
-        novel_id_list = cluster_db.get_dirfmtinfo_id_list(site_id, update_time)
-        self.logger.info('get {0} novel info'.format(len(novel_id_list)))
+        parser = SafeConfigParser()
+        parser.read('./conf/NovelClusterModule.conf')
+        self.start_site_id = parser.get('cluster_node_module', 'proc_start_site_id')
+        self.end_site_id = parser.get('cluster_node_module', 'proc_end_site_id')
 
-        for novel_id in novel_id_list:
-            novel_node = self.generate_novel_node(site_id, novel_id)
-            if novel_node is False:
-                continue
-            self.integrate_novel_node(novel_node)
-            self.update_novel_node(novel_node)
 
-    def update_novel_node(self, novel_node):
+    def init(self):
         """
-            单本小说信息更新到点集合当中
+            初始化基础信息，加载配置
         """
-        cluster_node = self.generate_cluster_node(novel_node.dir_id)
-        if cluster_node and novel_node.equal(cluster_node):
-            return False
+        self.init_log_info()
+        self.init_conf_info()
+        self.init_time_info()
+        self.logger.info('novel cluster node module init successful')
 
-        cluster_db = ClusterDBModule()
-        if cluster_node:
-            cluster_db.delete_novelclusterdirinfo(novel_node.dir_id)
-            cluster_db.delete_novelclusterchapterinfo_list(novel_node.dir_id)
 
-        self.logger.info('dir_id: {0}, table_id: {1}'.format(novel_node.dir_id, novel_node.dir_id % 256))
-        cluster_db.insert_novelclusterdirinfo(novel_node.dir_id, novel_node.generate_insert_tuple())
-        cluster_db.insert_novelclusterchapterinfo_list(novel_node.dir_id, [chapter.generate_insert_tuple() for chapter in novel_node.chapter_list])
-        return True
-
-    def generate_cluster_node(self, dir_id):
-        """
-            根据dir_id，获取点集合中已有的点信息
-        """
-        cluster_db = ClusterDBModule()
-
-        result = cluster_db.get_novelclusterdirinfo_info(dir_id)
-        if not result:
-            return False
-
-        (site_id, site, site_status, dir_id, dir_url, gid, book_name, pen_name) = result
-        book_name = book_name.decode('GBK', 'ignore')
-        pen_name = pen_name.decode('GBK', 'ignore')
-        cluster_node = NovelNodeInfo(site_id, site, site_status, dir_id, dir_url, gid, book_name, pen_name)
-
-        result = cluster_db.get_novelclusterchapterinfo_list(dir_id)
-        for (dir_id, chapter_id, chapter_sort, chapter_url, chapter_title, chapter_status) in result:
-            chapter_title = chapter_title.decode('GBK', 'ignore')
-            chapter = NovelChapterInfo(dir_id, chapter_id, chapter_sort, chapter_url, chapter_title, chapter_status)
-            cluster_node.chapter_list.append(chapter)
-        return cluster_node
-
-    def integrate_novel_node(self, novel_node):
-        """
-            单本小说信息的整合处理
-        """
-        for chapter in novel_node.chapter_list:
-            chapter.chapter_title = string_filter(chapter.chapter_title)
-
-    def generate_novel_node(self, site_id, novel_id):
+    def novel_node_collection(self, site_id, novel_id):
         """
             收集一本小说的基本信息
         """
         cluster_db = ClusterDBModule()
 
         result = cluster_db.get_dirfmtinfo_info(site_id, novel_id)
-        if result is False:
+        if not result:
             return False
-
         (site_id, site, site_status, dir_id, dir_url, gid, book_name, pen_name) = result
         book_name = book_name.decode('GBK', 'ignore')
         pen_name = pen_name.decode('GBK', 'ignore')
         novel_node = NovelNodeInfo(site_id, site, site_status, dir_id, dir_url, gid, book_name, pen_name)
 
         result = cluster_db.get_chapteroriinfo_list(site_id, dir_id)
+        if len(result) == 0:
+            return False
         for (dir_id, chapter_id, chapter_sort, chapter_url, chapter_title, chapter_status) in result:
             chapter_title = chapter_title.decode('GBK', 'ignore')
             chapter = NovelChapterInfo(dir_id, chapter_id, chapter_sort, chapter_url, chapter_title, chapter_status)
@@ -109,6 +77,57 @@ class ClusterNodeModule(object):
         return novel_node
 
 
+    def novel_node_integrate(self, novel_node):
+        """
+            单本小说信息的整合处理
+        """
+        for chapter in novel_node.chapter_list:
+            chapter.chapter_title = string_filter(chapter.chapter_title)
+        return True
+
+
+    def novel_node_update(self, novel_node):
+        """
+            单本小说信息更新到点集合当中
+        """
+        cluster_db = ClusterDBModule()
+
+        cluster_db.delete_novelclusterdirinfo(novel_node.dir_id)
+        cluster_db.delete_novelclusterchapterinfo_list(novel_node.dir_id)
+
+        cluster_db.insert_novelclusterdirinfo(novel_node.dir_id, novel_node.generate_insert_tuple())
+        cluster_db.insert_novelclusterchapterinfo_list(novel_node.dir_id, [chapter.generate_insert_tuple() for chapter in novel_node.chapter_list])
+        return True
+
+
+    def novel_node_generate(self, site_id, update_time):
+        """
+            更新site_id站点中，更新时间大于update_time的小说信息
+        """
+        cluster_db = ClusterDBModule()
+        novel_id_list = cluster_db.get_dirfmtinfo_id_list(site_id, update_time)
+
+        for index, novel_id in enumerate(novel_id_list):
+            novel_node = self.novel_node_collection(site_id, novel_id)
+            if not novel_node:
+                continue
+            self.novel_node_integrate(novel_node)
+            self.novel_node_update(novel_node)
+
+            if index % 100 == 0:
+                self.logger.info('[site_id: {0}, current_count: {1}, total_count: {2}]'.format(site_id, index, len(novel_id_list)))
+        return True
+
+
+    def run(self):
+        """
+        """
+        self.logger.info('novel cluster node module start')
+
+        for site_id in xrange(self.start_site_id, self.end_site_id + 1):
+            update_time = self.proc_time_dict['dir_fmt_info{0}'.format(site_id)]
+            self.novel_node_generate(site_id, update_time)
+        return True
 
 
 if __name__ == '__main__':
--- tools/test.py
+++ tools/test.py
@@ -11,7 +11,8 @@ def here():
 
 if __name__ == '__main__':
     for index in xrange(0, 256):
-        print('create table novel_cluster_edge_info{0} like novel_edge_chapter_info;'.format(index))
+        #print('create table novel_cluster_edge_info{0} like novel_edge_chapter_info;'.format(index))
+        print('dir_fmt_info{0}:0'.format(index))
     here()    
 
 
