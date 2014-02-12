489998ae27516654ae84449decd5664ac94cac39
sunhaowen <sunhaowen123@gmai.com>
2014-02-12 18:38:52 +0800

    novel node generate

--- tools/chapter_title.py
+++ tools/chapter_title.py
@@ -36,20 +36,21 @@ if __name__ == '__main__':
 
             sql = 'SELECT book_name, pen_name ' \
                   'FROM novel_cluster_dir_info{0} ' \
-                  'WHRER dir_id = {1}'.format(dir_id % 256, dir_id)
+                  'WHERE dir_id = {1}'.format(dir_id % 256, dir_id)
             cursor = conn.cursor()
             cursor.execute(sql)
             (book_name, pen_name) = cursor.fetchone()
             print('book_name: {0}, pen_name: {1}'.format(book_name, pen_name))
             cursor.close()
 
-            sql = 'SELECT chapter_title, raw_chapter_title ' \
+            sql = 'SELECT chapter_title, raw_chapter_title, chapter_sort ' \
                   'FROM novel_cluster_chapter_info{0} ' \
-                  'WHERE dir_di = {1}'.format(dir_id % 256, dir_id)
+                  'WHERE dir_id = {1} ' \
+                  'ORDER BY chapter_sort'.format(dir_id % 256, dir_id)
             cursor = conn.cursor()
             cursor.execute(sql)
-            for (chapter_title, raw_chapter_title, ) in cursor.fetchall():
-                print('chapter_title: {0}, raw_chapter_title: {1}'.format(chapter_title, raw_chapter_title))
+            for (chapter_title, raw_chapter_title, chapter_sort, ) in cursor.fetchall():
+                print('chapter_sort: {0}, chapter_title: {1}, raw_chapter_title: {2}'.format(chapter_sort, chapter_title, raw_chapter_title))
             cursor.close()
 
             print('---------------------------------------------------------------------------------------')
