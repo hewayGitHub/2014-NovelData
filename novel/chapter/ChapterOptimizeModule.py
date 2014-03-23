f4ef48bfe3d5fd87f2eb967aaad89859ce802f50
sunhaowen <sunhaowen123@gmai.com>
2014-03-23 16:21:20 +0800

    novel node generate

--- novel/chapter/ChapterOptimizeModule.py
+++ novel/chapter/ChapterOptimizeModule.py
@@ -209,16 +209,12 @@ class ChapterOptimizeModule(object):
 
         aggregate_dir_list = self.aggregate_dir_generate(rid)
         for (align_id, chapter_index) in aggregate_dir_list:
-            if chapter_index < 100:
-                continue
             candidate_chapter_list = self.candidate_chapter_generate(rid, align_id)
             candidate_chapter_list = self.candidate_chapter_filter(candidate_chapter_list)
             chapter = self.candidate_chapter_rank(candidate_chapter_list)
             print(chapter.chapter_title)
             print(chapter.chapter_url)
             print(chapter.chapter_content.encode('GBK', 'ignore'))
-            if chapter_index > 200:
-                break
 
 
     def run(self):
