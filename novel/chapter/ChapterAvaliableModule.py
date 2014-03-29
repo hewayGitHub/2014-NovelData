1ff6d106a50ebd839fc4cde0306bab6ff4a5606e
sunhaowen <sunhaowen123@gmai.com>
2014-03-29 19:14:28 +0800

    novel node generate

--- novel/chapter/ChapterAvaliableModule.py
+++ novel/chapter/ChapterAvaliableModule.py
@@ -25,29 +25,6 @@ class ChapterAvaliableModule(object):
         self.unavaliable_chapter_count = 0
 
 
-    def chapter_content_generate(self, chapter_url):
-        """
-            获取一个章节的正文信息
-        """
-        silk_server = SilkServer()
-
-        chapter_page = silk_server.get(src = chapter_url)
-        if not chapter_page:
-            return False
-        if not chapter_page.has_key('blocks'):
-            return False
-
-        raw_chapter_content = ''
-        for block in chapter_page['blocks']:
-            if block['type'] == 'NOVELCONTENT':
-                raw_chapter_content = block['data_value']
-        raw_chapter_content = html_filter(raw_chapter_content)
-        if len(raw_chapter_content) < 50:
-            return False
-
-        return raw_chapter_content
-
-
     def aggregate_dir_generate(self, rid):
         """
             获取一本小说的聚合目录
@@ -71,15 +48,7 @@ class ChapterAvaliableModule(object):
         for (chapter_index, chapter_id, chapter_url, optimize_chapter_status, optimize_chapter_wordsum) in aggregation_list:
             if optimize_chapter_wordsum > 0:
                 continue
-            chapter_content = self.chapter_content_generate(chapter_url)
-            if chapter_content is False:
-                unavaliable_count += 1
-                continue
-            chinese_count = 0
-            for char in chapter_content:
-                if is_chinese(char):
-                    chinese_count += 1
-            chapter_db.update_novelaggregationdir_wordsum(rid, chapter_id, 1, chinese_count)
+            unavaliable_count += 1
 
         self.total_chapter_count += len(aggregation_list)
         self.unavaliable_chapter_count += unavaliable_count
@@ -89,7 +58,6 @@ class ChapterAvaliableModule(object):
     def run(self):
         """
         """
-
         rid_list = []
         for line in open('./data/rid.txt', 'r').readlines():
             rid = int(line.strip())
