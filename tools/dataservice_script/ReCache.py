7ec4f6e3395e665acf36566df85af5ccebaff095
sunhaowen <sunhaowen123@gmai.com>
2014-04-02 21:57:46 +0800

    novel node generate

--- tools/dataservice_script/ReCache.py
+++ tools/dataservice_script/ReCache.py
@@ -4,48 +4,126 @@
 __author__ = 'sunhaowen'
 __date__ = '2014-04-02 20:25'
 
-import requesets
+import threading
+import requests
+import logging
+import time
+import json
 
 
 def here():
     print('PrimeMusic')
 
 
-def get(gid, nocache = 0):
+logger = logging.getLogger("work")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+class counter(object) :
+    def __init__(self, start = 0):
+        self.lock = threading.Lock()
+        self.value = start
+
+    def increment(self):
+        self.lock.acquire()
+        try:
+            self.value += 1
+        finally:
+            self.lock.release()
+
+
+def send_get_requests(url):
+    try :
+        result = requests.get(url).json()
+    except Exception, e :
+        return False
+    return result
+
+
+def get_gid(gid, nocache, error_count):
     """
     """
     src = 'http://m.baidu.com/open/dataservice/novel/aggregation/gid?lcid=mco_ds&query={0}&nocache={1}'.format(gid, nocache)
-    try:
-        result = requests.get(src).json()
-    except Exception, e:
-        print('Failed to request dataservice, exception: {0}, src: {1}'.format(e, src))
+    result = send_get_requests(src)
+
+    if result is False:
+        error_count.increment()
         return False
     if result.has_key('error_code'):
-        print('Failed to get data from dataservice, src: {0}'.format(src))
+        error_count.increment()
         return False
 
     return True
 
 
-if __name__ == '__main__':
+def get_dirurl(url, nocache, error_count):
+    """
+    """
+    src = 'http://m.baidu.com/open/dataservice/novel/aggregation/url?lcid=mco_ds&query={0}&nocache={1}'.format(url, nocache)
+    result = send_get_requests(src)
+
+    if result is False:
+        error_count.increment()
+        return False
+    if result.has_key('error_code'):
+        error_count.increment()
+        return False
+
+    return True
 
 
+def gid():
+    """
+    """
     rid_list = []
     for line in open('./data/rid.txt', 'r').readlines():
         rid = int(line.strip())
         rid_list.append(rid)
 
+    error_count = counter()
+    frequency = 5
     for index, rid in enumerate(rid_list):
-        result = get(rid, 0)
-        if not result:
-            print('error rid: {0}'.format(rid))
+        get_request = threading.Thread(name=index, target=get_gid, args=(rid, 0, error_count))
+        get_request.start()
+        if index % frequency == 0 :
+            time.sleep(1.0)
+
+    main_thread = threading.currentThread()
+    for t in threading.enumerate() :
+        if t is main_thread :
+            continue
+        t.join()
+    print(error_count.value, len(rid_list))
 
 
+def dirurl():
+    """
+    """
+    dir_url_list = []
+    for line in open('./data/dirurl.txt', 'r').readlines():
+        dir_url = int(line.strip())
+        dir_url_list.append(dir_url)
+
+    error_count = counter()
+    frequency = 5
+    for index, dir_url in enumerate(dir_url_list):
+        get_request = threading.Thread(name=index, target=get_dirurl, args=(dir_url, 0, error_count))
+        get_request.start()
+        if index % frequency == 0 :
+            time.sleep(1.0)
+
+    main_thread = threading.currentThread()
+    for t in threading.enumerate() :
+        if t is main_thread :
+            continue
+        t.join()
+    print(error_count.value, len(dir_url_list))
+
+
+if __name__ == '__main__':
+    gid()
+    #dirurl()
     here()
-
-
-
-
-
 
 
